1406745ad3cd9ae0130312aeec79df5f38378cf3
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-05-02 17:19:08 +0200

    Delete hci directory

--- hci/scripts/logging.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://server.kaikitagawajones.com", true);
-xhr.send(
-  '{"page":"' +
-    window.location.href +
-    '","alarms":' +
-    localStorage.getItem("alarms") +
-    "}"
-);
